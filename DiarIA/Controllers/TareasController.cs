bf294458dcf21e19421650cb6060f1260403ae7c
robertofabiot <rftercerom@uamv.edu.ni>
2025-12-07 05:16:11 -0600

    Multi-user CRUD working
    Add: scaffolding for CRUD tareas
    feat: user automaticaly assigned when creating a task

---
+++ DiarIA/Controllers/TareasController.cs
@@ -0,0 +1,175 @@
+﻿using DiarIA.Data;
+using DiarIA.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace DiarIA.Controllers
+{
+    [Authorize]
+    public class TareasController : Controller
+    {
+        private readonly DiarIAContext _context;
+
+        public TareasController(DiarIAContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Tareas
+        public async Task<IActionResult> Index()
+        {
+            // Obtener el ID del usuario logueado
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            // Filtrar: Traer solo las tareas donde UserId == mi ID
+            var misTareas = await _context.Tareas
+                                          .Where(t => t.UserId == userId)
+                                          .ToListAsync();
+
+            return View(misTareas);
+        }
+
+        // GET: Tareas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tarea = await _context.Tareas
+                .Include(t => t.Usuario)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (tarea == null)
+            {
+                return NotFound();
+            }
+
+            return View(tarea);
+        }
+
+        // GET: Tareas/Create
+        public IActionResult Create()
+        {
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Tareas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Nombre,Descripcion,FechaTope,FechaAgendada,DuracionMinutos,Prioridad,Dificultad,Completada,UserId")] Tarea tarea)
+        {
+            if (ModelState.IsValid)
+            {
+                tarea.UserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+                _context.Add(tarea);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tarea);
+        }
+
+        // GET: Tareas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tarea = await _context.Tareas.FindAsync(id);
+            if (tarea == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", tarea.UserId);
+            return View(tarea);
+        }
+
+        // POST: Tareas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nombre,Descripcion,FechaTope,FechaAgendada,DuracionMinutos,Prioridad,Dificultad,Completada,UserId")] Tarea tarea)
+        {
+            if (id != tarea.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tarea);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TareaExists(tarea.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", tarea.UserId);
+            return View(tarea);
+        }
+
+        // GET: Tareas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tarea = await _context.Tareas
+                .Include(t => t.Usuario)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (tarea == null)
+            {
+                return NotFound();
+            }
+
+            return View(tarea);
+        }
+
+        // POST: Tareas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var tarea = await _context.Tareas.FindAsync(id);
+            if (tarea != null)
+            {
+                _context.Tareas.Remove(tarea);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TareaExists(int id)
+        {
+            return _context.Tareas.Any(e => e.Id == id);
+        }
+    }
+}
